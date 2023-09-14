1efb17c17701ffc3f006e88c4bab68b9fb387482
Binnur KURT <deepcloudlabs@gmail.com>
2023-09-14 11:18:26 +0300

    initial commit

---
+++ module06-functional.programming/exercise01.py
@@ -0,0 +1,15 @@
+"""
+ I. Imperative programming: Procedural/OOP
+    Problem -> Algorithm -> Implementation -> Solution
+    External Loop
+ II. Declarative Programming: Functional Programming/OOP
+    Problem -> Description of the solution -> Solution
+
+    Programming Paradigms:
+     i. Procedural Programming (like in C/C++/Go/Fortran/Python/Php/...)
+    ii. OOP (like C++/C#/Java/...)
+   iii. Functional Programming
+    iv. AOP
+     V. Generic Programming (like C++)
+    vi. Reactive Programming (like Java)(using libraries in C++/JS/C#/...)
+"""
---
+++ module06-functional.programming/exercise02.py
@@ -0,0 +1,7 @@
+"""
+Procedural Programming: function      -> hierarchical
+OOP                   : class/object  -> object network
+Functional Programming: a) Higher-Order Function
+                        b) Pure Function
+                        Pipeline -> HoF composition
+"""
