269764d541c33a20db63146905446c03be360ae8
Binnur KURT <deepcloudlabs@gmail.com>
2023-09-14 14:37:43 +0300

    initial commit

---
+++ module07-exceptions.and.file.operations/exercise01.py
@@ -0,0 +1,5 @@
+"""
+Failure
+1. Type  I Error -> Fix
+2. Type II Error -> UI |-> User / DB / File
+"""
---
+++ module07-exceptions.and.file.operations/exercise02.py
@@ -0,0 +1,14 @@
+"""
+Persistence -> Disk -> File Systems -> File: Abstraction
+Data -> File Organization
+ i) Text I/O   : HRF -> CSV
+ii) Binary I/O : Efficiency
+
+"""
+u = 3615  # u -> ...|3|6|1|5|...
+u = 42  # u -> ...|4|2|...
+
+numbers = [4, 8, 15, 16, 23, 42]
+"""
+  4|8|1|5|1|6|2|3|4|2
+"""
