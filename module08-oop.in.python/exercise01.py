5e28230202aabf47fdd0c031f61921ee5a6ae4c3
Binnur KURT <deepcloudlabs@gmail.com>
2023-09-15 09:00:14 +0300

    initial commit

---
+++ module08-oop.in.python/exercise01.py
@@ -0,0 +1,9 @@
+"""
+OOP:
+I) Abstraction: Class -> Object
+                encapsulation: data/state <-- method
+                design principle: information hiding
+II) Inheritance -> Re-usability
+III) Polymorphism -> Agility
+                     Open/Closed Principle
+"""
