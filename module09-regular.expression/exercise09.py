e1e904fe855c6bebcc77fcb928e8dd8cbada6227
Binnur KURT <deepcloudlabs@gmail.com>
2023-09-15 15:42:44 +0300

    Update exercise09.py

--- module09-regular.expression/exercise09.py
+++ module09-regular.expression/exercise09.py
@@ -3,9 +3,9 @@ words longer than 12-chars in turkish dictionary
 """
 import re
 
-pattern = "???"
+pattern = "\w{12,}"
 
 with open("dictionary-tur.txt", "rt") as tur_dic:
     for word in tur_dic.readlines():
-        if re.fullmatch(pattern, word):
-            print(word)
+        if re.fullmatch(pattern, word.strip()):
+            print(word.strip())
